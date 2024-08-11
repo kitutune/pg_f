d84c409096ae1b86ff0090efc0de5d4cf5ed0fac
kitutune <p.kitutune@gmail.com>
2024-08-11 14:57:16 +0900

    ：testファイルでjestの型が正確に読み込まれない対応

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,13 @@
-import type {Config} from 'jest';
+import type { Config } from "jest";
 
 const config: Config = {
-  verbose: true,
+  preset: "ts-jest",
+  testEnvironment: "jsdom", // ブラウザ環境を模倣するための設定
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"], // 追加のセットアップファイルを指定
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy", // CSSモジュールのマッピング
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"], // サポートする拡張子のリスト
 };
 
 export default config;
