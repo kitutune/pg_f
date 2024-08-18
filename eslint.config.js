8f3f7ededd6b0f52b91b201ea686bfe109135fdc
kitutune <p.kitutune@gmail.com>
2024-08-18 17:33:56 +0900

    eslintの無駄を撤去

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,17 @@
 import globals from "globals"; // ブラウザ環境のグローバル変数をESLintで認識させるための設定
-import js from "@eslint/js"; // ESLintの標準JavaScriptルールセットを提供するパッケージ
+import pluginJs from "@eslint/js"; // ESLintの標準JavaScriptルールセットを提供するパッケージ
 import tseslint from "typescript-eslint"; // TypeScript用のESLintルールとパーサーを提供するパッケージ
 import pluginReact from "eslint-plugin-react"; // React用のESLintルールセットを提供するパッケージ
-import eslintConfigPrettier from "eslint-config-prettier"; // PrettierとESLintの競合を防ぐための設定
-import pluginPrettier from "eslint-plugin-prettier"; // PrettierのルールをESLintに統合し、ESLintがPrettierのルールをチェックできるようにするプラグイン
 
 export default [
-  { 
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], // 対象とするファイルのパターンを指定。JavaScript、TypeScript、JSX、TSXファイルが対象
-  },
-  { 
-    ignores: ["dist"], // ESLintが無視するディレクトリを指定。ここではdistディレクトリが対象
-  },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] }, // 対象とするファイルのパターンを指定。JavaScript、TypeScript、JSX、TSXファイルが対象
+  { languageOptions: { globals: globals.browser } }, // ブラウザのグローバル変数（例：window, document）をESLintに認識させる
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
   {
-    languageOptions: {
-      parser: '@typescript-eslint/parser', // TypeScriptコードを解析するためのパーサーを指定
-      globals: globals.browser, // ブラウザのグローバル変数（例：window, document）をESLintに認識させる
-    },
-  },
-  js.configs.recommended, // JavaScriptのベストプラクティスに従った推奨ルールセットを適用
-  ...tseslint.configs.recommended, // TypeScriptに特化した推奨ルールセットを適用
-  pluginReact.configs.flat.recommended, // Reactのベストプラクティスに従った推奨ルールセットを適用
-  eslintConfigPrettier, // ESLintとPrettierの競合を防ぐ設定。Prettierのルールを最優先にするための設定
-  {
-    plugins: [pluginPrettier], // PrettierプラグインをESLintに追加
     rules: {
-      "prettier/prettier": "error", // Prettierのフォーマットルール違反をESLintでエラーとして扱う
+      "no-console": "error", // 試し
     },
   },
 ];
